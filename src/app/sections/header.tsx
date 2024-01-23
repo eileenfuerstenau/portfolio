961a90f5f3f84f798a54bf6448c6bd1eea8b19ce
Eileen Fürstenau <eileen.fuerstenau@maibornwolff.de>
2024-01-23 14:49:16 +0100

    add sizes prop to image

--- src/app/sections/header.tsx
+++ src/app/sections/header.tsx
@@ -11,15 +11,18 @@ export default function Header() {
         alt="Page Header"
         style={{
           maxWidth: "100%",
-          height: "auto"
-        }} />
+          height: "auto",
+          width: "auto",
+        }}
+      />
       <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 h-3/5">
         <Image
           src={"/me.png"}
           alt="Picture of Author"
           className="object-contain !relative"
           fill
-          sizes="100vw" />
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
       </div>
     </div>
   );
